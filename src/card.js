71f5e3c9b9e110231c3edc940ae42ef079cfd903
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-01 20:55:49 +0530

    Updated card component

--- src/card.js
+++ src/card.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import Radio from './radio';
 
 class Card extends Component {
     state = {  }
     render() { 
         return ( 
-            <div class="card" style={{width: 300,height: 450}}>
-                <img class="card-img-top" src={this.props.pic} style={{height: 200}} alt="Card image cap"></img>
+            <div class="card card-spacing" style={{width: 300,height: 450}}>
                 <div class="card-body">
-                    <h5 class="card-title">{this.props.title}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title text-md-center mt-2 mb-5">{this.props.title}</h5>
+                    <ul>
+                        <Radio name="Linear Search" type="ls"/>
+                        <Radio name="Binary Search" type="bs"/>
+                        <Radio name="Bubble Sort" type="bsort"/>
+                        <Radio name="Insertion Sort" type="isort"/>
+
+                    </ul>
                 </div>
             </div>
          );
