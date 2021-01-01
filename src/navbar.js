c106559d944277848ba1a4d1b842ea8e58b713d0
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-01 21:19:11 +0530

    Updated Navbar component

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     state = {  }
     render() { 
         return ( 
-        <nav className="navbar bg-danger">
+        <nav className="navbar bg-primary">
             <a href="#" className="navbar-brand text-white offset-5"><center>Data Structure Algo Visualization</center></a>
         </nav>
         );
