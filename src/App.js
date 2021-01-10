ec16160e02e773b077872f3c0e8feb3193828a63
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-10 19:47:16 +0530

    Modified React app for this repository

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
-import Card from './card';
-import Jumbotron from './jumbotron';
-import Navbar from './navbar';
+import React from "react";
 
-class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <div>
-        <Navbar/>
-        <Jumbotron/>
-        <Card title="Selection of Algo for visualization"/>
-      </div>
-     );
-  }
+import NavBar from "./utils/navbar";
+import { Route } from "react-router-dom";
+
+//  IndexTable
+import IndexTable from "./utils/indexPage";
+
+//  Sorting Visualiser
+import SortingVisualizer from "./sortingAlgorithms/sortingVisualizer";
+
+//  Searching Visualiser
+import BinarySearch from "./searchingAlgorithms/binarySearch/binarySearch";
+import LinearSearch from "./searchingAlgorithms/linearSearch/linearSearch";
+
+//  Stylesheets
+import "./App.css";
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <Route exact path="/" component={IndexTable} />
+                <Route path="/sorting" component={SortingVisualizer} />
+                <Route path="/linear-search" component={LinearSearch} />
+                <Route path="/binary-search" component={BinarySearch} />
+            </div>
+        );
+    }
 }
- 
-export default App;
