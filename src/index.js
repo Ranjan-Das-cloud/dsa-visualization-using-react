8edd65df162fc65e65e9a842087d896aa1199184
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-10 19:49:13 +0530

    Added index.js file to this repo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { HashRouter as Router } from "react-router-dom";
+
+// HashRouter basically it uses the hash in the URL to render the component. Since I was building a static one-page website, I needed to use this.
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
