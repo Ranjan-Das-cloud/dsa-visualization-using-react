85c5a4b466822369117fb15376344b198a002b48
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-14 21:48:50 +0530

    Updated Footer component of React App

--- src/sortingAlgorithms/components/subComponents/Footer/index.js
+++ src/sortingAlgorithms/components/subComponents/Footer/index.js
@@ -1,41 +1,83 @@
 import React from 'react';
-import './style.css';
 
-const Footer = (props) => {
+//  import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+const Footer = () => {
   return (
-    <footer className="Footer">
-      <section>
-         Demonstrated and Created with{' '}
-        <span className="Footer__Heart">&hearts;</span> by{' '}
-        <a
-          href="https://www.linkedin.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__Link"
-        >
-          Ranjan Das
-        </a>
-      </section>
+    <footer class="bg-primary text-white text-center text-lg-start">
+  
+  <div class="container p-4">
+    
+    <div class="row">
 
-      <section className="Footer__Items">
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__Link"
-        >
-          Demo
-        </a>
-        <a
-          href="https://github.com/Ranjan-Das-cloud"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__Link"
-        >
-          Code
-        </a>
-      </section>
-    </footer>
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">About Me</h5>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+          voluptatem veniam, est atque cumque eum delectus sint!
+          Demonstrated with{' '}
+              <span className="Footer__Heart">&hearts;</span> by{' '}
+              <a
+                href="https://www.github.com/Ranjan-Das-cloud"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="Footer__Link"
+              >
+                Ranjan Das
+              </a>
+        </p>
+      </div>
+      
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Downloads</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-white">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 4</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Contacts</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-white">Link 1</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 2</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+    
+  </div>
+  
+  <div class="text-center p-3" style={{background: 'rgba(0, 0, 0, 0.2)'}}>
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">Ranjan-Das-cloud</a>
+  </div>
+  
+</footer>
   );
 };
 
