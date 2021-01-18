d19a69c817af3ee4381771cfca66c0498261382a
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-18 20:26:19 +0530

    Modified styles of buttons of navbar

--- src/utils/navbar.jsx
+++ src/utils/navbar.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const NavBar = () => (
     <div className="bg-dark py-2 px-2">
         <span>
-            <a className="btn-sm text-decoration-none bg-dark rounded-0 text-light" href="https://github.com/ranjandas/dsa-algorithms-visualiser">
+            <a className="btn-sm text-decoration-none bg-secondary rounded text-light" href="https://github.com/Ranjan-Das-cloud/dsa-algorithms-visualiser">
                 <i className="fab text-light fa-github"></i>&nbsp; Source Code
             </a>
-            <a className="btn-sm text-decoration-none bg-dark rounded-0 text-light" href="https://linkedin.com/in/ranjandas">
+            <a className="btn-sm text-decoration-none bg-primary rounded float-right text-light" href="https://linkedin.com/in/">
                 <i className="fab text-light fa-linkedin"></i>
                 &nbsp; Demonstrated by Ranjan Das
             </a>
