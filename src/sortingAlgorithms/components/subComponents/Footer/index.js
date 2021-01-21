2ba305f65996fd23b776124fb396f6ee82850907
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-21 22:16:28 +0530

    Updated Footer component for React App

--- src/sortingAlgorithms/components/subComponents/Footer/index.js
+++ src/sortingAlgorithms/components/subComponents/Footer/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 //  import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
-import {
-  
-} from react-icons/md;
+import './node_modules/@'
+
 
 const Footer = () => {
   return (
@@ -40,13 +39,13 @@ const Footer = () => {
 
             <ul class="list-unstyled mb-0">
               <li>
-                <a href="https://www.geeksforgeeks.org/sorting-algorithms/" rel="noopener noreferrer" target="_blank" class="text-white">Geek for Geeks</a>
+                <a href="https://www.geeksforgeeks.org/sorting-algorithms/" rel="noopener noreferrer" target="_blank" class="hover-me text-white">GeekforGeeks</a>
               </li>
               <li>
                 <a href="https://www.javatpoint.com/data-structure-tutorial" rel="noopener noreferrer" target="_blank" class="text-white">Javatpoint</a>
               </li>
               <li>
-                <a href="https://www.hackerrank.com" rel="noopener noreferrer" target="_blank" class="text-white">Hacker Rank</a>
+                <a href="https://www.hackerrank.com" rel="noopener noreferrer" target="_blank" class="text-white">HackerRank</a>
               </li>
               <li>
                 <a href="https://www.hackerearth.com" rel="noopener noreferrer" target="_blank" class="text-white">HackerEarth</a>
@@ -59,16 +58,16 @@ const Footer = () => {
 
             <ul class="list-unstyled mb-0">
               <li>
-                <a href="https://github.com/Ranjan-Das-cloud" target="_blank" rel="noopener noreferrer" class="text-white">Github</a>
+                <a href="https://github.com/Ranjan-Das-cloud" target="_blank" rel="noopener noreferrer" class="text-white"><i className="fab text-light hover-me  fa-lg fa-github"></i></a>
               </li>
               <li>
-                <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer" class="text-white">LinkedIn</a>
+                <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer" class="text-white"><i className="fab text-light fa-lg fa-linkedin"></i></a>
               </li>
               <li>
-                <a href="https://gmail.com/" target="_blank" rel="noopener noreferrer" class="text-white">Email</a>
+                <a href="https://gmail.com/" target="_blank" rel="noopener noreferrer" class="text-white"><i className="fab text-light fa-lg fa-paper-plane"></i></a>
               </li>
               <li>
-                <a href="https://www.medium.com/@ranjan.das" target="_blank" rel="noopener noreferrer" class="text-white">Blogs</a>
+                <a href="https://www.medium.com/@ranjan.das" target="_blank" rel="noopener noreferrer" class="text-white"><i className="fab text-light fa-lg fa-medium"></i></a>
               </li>
             </ul>
           </div>
@@ -76,10 +75,67 @@ const Footer = () => {
         </div>
         
       </div>
+
+      <div class="mt-5">
+          <div class='container has-text-centered'> 
+          <div class='columns is-mobile is-centered'> 
+            <div class='column is-5'> 
+              <div> 
+                <h1 class='title has-text-centered'> 
+                    Contact Me 
+                </h1> 
+              </div> 
+      
+              <div class='box'> 
+                <div> 
+                  <span class="icon is-large"> 
+                    <span class="fa-stack fa-lg"> 
+                      <i class="fas fa-github  
+                            fa-stack-1x"></i> 
+                       
+                    </span> 
+                  </span> 
+      
+                  <span class="icon is-large">
+                    <span class="fa-stack fa-lg"> 
+                    
+                      <i class="fas fa-linkedin  
+                        fa-stack-1x has-text-white"></i> 
+                    </span> 
+                  </span> 
+      
+                  <span class="icon is-large"> 
+                    <span class="fa-stack fa-lg"> 
+                       
+                      <a href="www.google.com" target="_blank" rel="noopener noreferrer" class="text-white"><i class="fas fa-phone-alt fa-stack-1x has-text-white"></i></a>
+                    </span> 
+                  </span> 
+      
+                  <span class="icon is-large"> 
+                    <span class="fa-stack fa-lg"> 
+                       
+                      <i class="fab fa-medium  
+                        fa-stack-1x has-text-white"></i>
+                    </span> 
+                  </span> 
+      
+                  <span class="icon is-large"> 
+                    <span class="fa-stack fa-lg"> 
+                       
+                      <i class="fas fa-paper-plane  
+                        fa-stack-1x has-text-white"></i>
+                    </span> 
+                  </span> 
+                </div> 
+              </div> 
+            </div> 
+          </div> 
+        </div>
+      </div>
       
       <div class="text-center p-3" style={{background: 'rgba(0, 0, 0, 0.2)'}}>
         © 2020 Copyright :&nbsp;&nbsp;
-        <a class="text-white" href="https://github.com/Ranjan-Das-cloud">Ranjan-Das-cloud</a><span>&nbsp;&nbsp;&#9819;</span>
+        <a class="text-white" href="https://github.com/Ranjan-Das-cloud">Ranjan-Das-cloud</a><span>&nbsp;&nbsp;<i class="fas fa-chess-king"></i></span>
       </div>
       
     </footer>
