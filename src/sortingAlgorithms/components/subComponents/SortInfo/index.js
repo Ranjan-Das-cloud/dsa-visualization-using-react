1923fab02129449ba1f23113a1a0a44598db7fc4
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-23 20:42:27 +0530

    Updated icons for complexity of algorithms

--- src/sortingAlgorithms/components/subComponents/SortInfo/index.js
+++ src/sortingAlgorithms/components/subComponents/SortInfo/index.js
@@ -13,7 +13,7 @@ const SortInfo = ({
   return (
     <div className="SortInfo">
       <hr />
-      <h1><i class="cil-sad"></i>&nbsp;{title ? title : 'Select Algorithm'}&nbsp;&nbsp;<i class="cil-screen-desktop"></i></h1>
+      <h1><i class="cil-mood-very-good"></i>&nbsp;{title ? title : 'Select Algorithm'}</h1>
 
       <div className="SortInfo__Body">
         <article className="SortInfo_desc">
@@ -21,7 +21,7 @@ const SortInfo = ({
             description
           ) : (
             <p>
-              You have to select an algorithm before you can visualize it's behavioral execution over an array.
+              <span><i class="cil-sad"></i>&nbsp;</span>You have to select an algorithm before you can visualize it's behavioral execution over an array.
             </p>
           )}
         </article>
@@ -31,28 +31,28 @@ const SortInfo = ({
           <table>
             <tbody>
               <tr>
-                <td><span><i class="cid-chart-line"></i>&nbsp;</span>Worst-case time complexity</td>
+                <td><span><i class="cil-chart-line"></i>&nbsp;</span>Worst-case time complexity</td>
                 <td>
                   <code>{worstCase}</code>
                 </td>
               </tr>
 
               <tr>
-                <td><span><i class="cid-chart-line"></i>&nbsp;</span>Average time complexity</td>
+                <td><span><i class="cil-chart-line"></i>&nbsp;</span>Average time complexity</td>
                 <td>
                   <code>{avgCase}</code>
                 </td>
               </tr>
 
               <tr>
-                <td><span><i class="cid-chart-line"></i>&nbsp;</span>Best-case time complexity</td>
+                <td><span><i class="cil-chart-line"></i>&nbsp;</span>Best-case time complexity</td>
                 <td>
                   <code>{bestCase}</code>
                 </td>
               </tr>
 
               <tr>
-                <td><span><i class="cid-chart-area"></i>&nbsp;</span>Worst-case space complexity</td>
+                <td><span><i class="cis-chart-area"></i>&nbsp;</span>Worst-case space complexity</td>
                 <td>
                   <code>{space}</code>
                 </td>
