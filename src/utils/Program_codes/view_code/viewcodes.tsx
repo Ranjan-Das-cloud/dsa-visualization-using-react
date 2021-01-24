9ee8db36b8edfc7e17c6cc5894c2787108a09ee8
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-24 12:09:55 +0530

    Added react component for program code container

---
+++ src/utils/Program_codes/view_code/viewcodes.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import { Divider } from "@react-md/divider";
+import { Text } from "@react-md/typography";
+
+import CrossFadeExamplesAsync from "./CrossFadeElementAsync";
+//import CrossFadeExamplesStatic from "./CrossFadeExamplesStatic";
+
+const Viewcodes: FC = () => (
+  <>
+    <Text type="headline-6" margin="bottom">
+      Static Transitions
+    </Text>
+    
+    <Divider />
+    <Text type="headline-6">
+      Async with <code>Suspense</code>
+    </Text>
+    <CrossFadeExamplesAsync />
+    <Divider />
+  </>
+);
+
+export default Viewcodes;
