e6526641cb173466783b86d5cc49f52ca2a4941c
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-02-01 04:20:39 +0530

    Added source code of Bubble Sort in C# language

--- src/utils/Program_codes/view_code/SortingAlgorithms/BubbleSort/CodingwithC#.tsx
+++ src/utils/Program_codes/view_code/SortingAlgorithms/BubbleSort/CodingwithC#.tsx
@@ -2,19 +2,113 @@ import React, { forwardRef, HTMLAttributes } from "react";
 import { Text } from "@react-md/typography";
 
 export default forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  function PageB6(props, ref) {
+  function PageBu5(props, ref) {
     return (
       <div {...props} ref={ref} className="mt-3 mb-1">
         <Text type="headline-4">Source code in C#</Text>
         <Text>
           <code>
-          <div className="container text-left offset-4"><h5 className="mt-5 mb-4"><span><h4 className="text-success"><b>Recursive</b> implementation of Binary Search</h4></span>
-          </h5>
-          
-          <h5>
-
-          </h5>
-          </div>
+            <h5 className="mt-5 mb-4"><span><h4 style={{color:'#f0ffff'}}>Implementation of Bubble Sort</h4></span>
+              <div className="container text-left">
+                <div className="offset-3 mt-4 mb-5">
+                  <div className="line number1 index0 alt2"><code className="comments">&#47;&#47; C# program for implementation&nbsp; </code></div>
+                  <div className="line number2 index1 alt1"><code className="comments">&#47;&#47; of Bubble Sort </code></div>
+                  <div className="line number3 index2 alt2"><code className="keyword">using</code> <code className="plain">System; </code></div>
+                  <div className="line number4 index3 alt1"><code className="undefined spaces">&nbsp;</code>&nbsp;</div>
+                  <div className="line number5 index4 alt2"><code className="keyword">class</code> <code className="plain">GFG </code></div>
+                  <div className="line number6 index5 alt1"><code className="plain">&#123;&nbsp; </code></div>
+                  <div className="line number7 index6 alt2 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">static</code> <code className="keyword">void</code> <code className="plain">bubbleSort(</code><code className="keyword">int</code> <code className="plain">[]arr) </code></div>
+                  <div className="line number8 index7 alt1 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#123; </code></div>
+                  <div className="line number9 index8 alt2 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">int</code> <code className="plain">n = arr.Length; </code></div>
+                  <div className="line number10 index9 alt1 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">for</code> <code className="plain">(</code><code className="keyword">int</code> <code className="plain">i = 0; i &lt; n - 1; i++) </code></div>
+                  <div className="line number11 index10 alt2 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">for</code> <code className="plain">(</code><code className="keyword">int</code> <code className="plain">j = 0; j &lt; n - i - 1; j++) </code></div>
+                  <div className="line number12 index11 alt1 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">if</code> <code className="plain">(arr[j] &gt; arr[j + 1]) </code></div>
+                  <div className="line number13 index12 alt2 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#123; </code></div>
+                  <div className="line number14 index13 alt1 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; swap temp and arr[i] </code></div>
+                  <div className="line number15 index14 alt2 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">int</code> <code className="plain">temp = arr[j]; </code></div>
+                  <div className="line number16 index15 alt1 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">arr[j] = arr[j + 1]; </code></div>
+                  <div className="line number17 index16 alt2 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">arr[j + 1] = temp; </code></div>
+                  <div className="line number18 index17 alt1 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#125; </code></div>
+                  <div className="line number19 index18 alt2 highlighted"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#125; </code></div>
+                  <div className="line number20 index19 alt1"><code className="undefined spaces">&nbsp;</code>&nbsp;</div>
+                  <div className="line number21 index20 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#8727; Prints the array &#8727;&#47;</code></div>
+                  <div className="line number22 index21 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">static</code> <code className="keyword">void</code> <code className="plain">printArray(</code><code className="keyword">int</code> <code className="plain">[]arr) </code></div>
+                  <div className="line number23 index22 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#123; </code></div>
+                  <div className="line number24 index23 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">int</code> <code className="plain">n = arr.Length; </code></div>
+                  <div className="line number25 index24 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">for</code> <code className="plain">(</code><code className="keyword">int</code> <code className="plain">i = 0; i &lt; n; ++i) </code></div>
+                  <div className="line number26 index25 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">Console.Write(arr[i] + </code><code className="string">" "</code><code className="plain">); </code></div>
+                  <div className="line number27 index26 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">Console.WriteLine(); </code></div>
+                  <div className="line number28 index27 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#125; </code></div>
+                  <div className="line number29 index28 alt2"><code className="undefined spaces">&nbsp;</code>&nbsp;</div>
+                  <div className="line number30 index29 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; Driver method </code></div>
+                  <div className="line number31 index30 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">public</code> <code className="keyword">static</code> <code className="keyword">void</code> <code className="plain">Main() </code></div>
+                  <div className="line number32 index31 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#123; </code></div>
+                  <div className="line number33 index32 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">int</code> <code className="plain">[]arr = &#123;64, 34, 25, 12, 22, 11, 90&#125;; </code></div>
+                  <div className="line number34 index33 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">bubbleSort(arr); </code></div>
+                  <div className="line number35 index34 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">Console.WriteLine(</code><code className="string">"Sorted array"</code><code className="plain">); </code></div>
+                  <div className="line number36 index35 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">printArray(arr); </code></div>
+                  <div className="line number37 index36 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#125; </code></div>
+                  <div className="line number38 index37 alt1"><code className="undefined spaces">&nbsp;</code>&nbsp;</div>
+                  <div className="line number39 index38 alt2"><code className="plain">&#125; </code></div>
+                  <div className="line number40 index39 alt1"><code className="undefined spaces">&nbsp;</code>&nbsp;</div>
+                </div>
+              </div>
+            </h5>
+            <h5 className="mt-5 mb-4"><span><h4 style={{color:'#f0ffff'}}><b>Recursive </b>Implementation of Bubble Sort</h4></span>
+              <div className="container text-left">
+                <div className="offset-3 mt-4 mb-5">
+                  <div className="line number1 index0 alt2"><code className="comments">&#47;&#47; C# program for recursive </code></div>
+                  <div className="line number2 index1 alt1"><code className="comments">&#47;&#47; implementation of Bubble sort</code></div>
+                  <div className="line number3 index2 alt2"><code className="keyword">using</code> <code className="plain">System;</code></div>
+                  <div className="line number4 index3 alt1">&nbsp;</div>
+                  <div className="line number5 index4 alt2"><code className="keyword">class</code> <code className="plain">GFG</code></div>
+                  <div className="line number6 index5 alt1"><code className="plain">&#123;</code></div>
+                  <div className="line number7 index6 alt2">&nbsp;</div>
+                  <div className="line number8 index7 alt1"><code className="comments">&#47;&#47; A function to implement</code></div>
+                  <div className="line number9 index8 alt2"><code className="comments">&#47;&#47; bubble sort</code></div>
+                  <div className="line number10 index9 alt1"><code className="keyword">static</code> <code className="keyword">void</code> <code className="plain">bubbleSort(</code><code className="keyword">int</code> <code className="plain">[]arr,&nbsp;&nbsp; </code></div>
+                  <div className="line number11 index10 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">int</code> <code className="plain">n)</code></div>
+                  <div className="line number12 index11 alt1"><code className="plain">&#123;</code></div>
+                  <div className="line number13 index12 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; Base case</code></div>
+                  <div className="line number14 index13 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">if</code> <code className="plain">(n == 1)</code></div>
+                  <div className="line number15 index14 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">return</code><code className="plain">;</code></div>
+                  <div className="line number16 index15 alt1">&nbsp;</div>
+                  <div className="line number17 index16 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; One pass of bubble </code></div>
+                  <div className="line number18 index17 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; sort. After this pass,</code></div>
+                  <div className="line number19 index18 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; the largest element</code></div>
+                  <div className="line number20 index19 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; is moved (or bubbled) </code></div>
+                  <div className="line number21 index20 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; to end.</code></div>
+                  <div className="line number22 index21 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">for</code> <code className="plain">(</code><code className="keyword">int</code> <code className="plain">i = 0; i &lt; n - 1; i++)</code></div>
+                  <div className="line number23 index22 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">if</code> <code className="plain">(arr[i] &gt; arr[i + 1])</code></div>
+                  <div className="line number24 index23 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#123;</code></div>
+                  <div className="line number25 index24 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; swap arr[i], arr[i+1]</code></div>
+                  <div className="line number26 index25 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">int</code> <code className="plain">temp = arr[i];</code></div>
+                  <div className="line number27 index26 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">arr[i] = arr[i + 1];</code></div>
+                  <div className="line number28 index27 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">arr[i + 1] = temp;</code></div>
+                  <div className="line number29 index28 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">&#125;</code></div>
+                  <div className="line number30 index29 alt1">&nbsp;</div>
+                  <div className="line number31 index30 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; Largest element is fixed,</code></div>
+                  <div className="line number32 index31 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="comments">&#47;&#47; recur for remaining array</code></div>
+                  <div className="line number33 index32 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">bubbleSort(arr, n - 1);</code></div>
+                  <div className="line number34 index33 alt1"><code className="plain">&#125;</code></div>
+                  <div className="line number35 index34 alt2">&nbsp;</div>
+                  <div className="line number36 index35 alt1"><code className="comments">&#47;&#47; Driver code</code></div>
+                  <div className="line number37 index36 alt2"><code className="keyword">static</code> <code className="keyword">void</code> <code className="plain">Main()</code></div>
+                  <div className="line number38 index37 alt1"><code className="plain">&#123;</code></div>
+                  <div className="line number39 index38 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">int</code> <code className="plain">[]arr = &#123;64, 34, 25, </code></div>
+                  <div className="line number40 index39 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">12, 22, 11, 90&#125;;</code></div>
+                  <div className="line number41 index40 alt2">&nbsp;</div>
+                  <div className="line number42 index41 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">bubbleSort(arr, arr.Length);</code></div>
+                  <div className="line number43 index42 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code>&nbsp;</div>
+                  <div className="line number44 index43 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">Console.WriteLine(</code><code className="string">"Sorted array : "</code><code className="plain">);</code></div>
+                  <div className="line number45 index44 alt2"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="keyword">for</code><code className="plain">(</code><code className="keyword">int</code> <code className="plain">i = 0; i &lt; arr.Length; i++)</code></div>
+                  <div className="line number46 index45 alt1"><code className="undefined spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code className="plain">Console.Write(arr[i] + </code><code className="string">" "</code><code className="plain">);</code></div>
+                  <div className="line number47 index46 alt2"><code className="plain">&#125;</code></div>
+                  <div className="line number48 index47 alt1"><code className="plain">&#125;</code></div>
+                  <div className="line number49 index48 alt2">&nbsp;</div>
+                </div>
+              </div>
+            </h5>
           </code>
         </Text>
       </div>
