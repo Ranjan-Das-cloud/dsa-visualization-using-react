4bdd02ddb552d4fa22ce9b0b1d8c187b44f78ee9
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-01-31 16:29:05 +0530

    Added source code of Merge Sort in C language

--- src/utils/Program_codes/view_code/SortingAlgorithms/MergeSort/CodingwithC.tsx
+++ src/utils/Program_codes/view_code/SortingAlgorithms/MergeSort/CodingwithC.tsx
@@ -8,7 +8,7 @@ export default forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
         <Text type="headline-4">Source code in C</Text>
         <Text>
           <code>
-            <h5 className="mt-5 mb-4"><span><h4 style={{color:'#f0ffff'}}>Implementation of Quick Sort</h4></span>
+            <h5 className="mt-5 mb-4"><span><h4 style={{color:'#f0ffff'}}>Implementation of Merge Sort</h4></span>
               <div className="container text-left">
                 <div className="offset-3 mt-4 mb-5">
                   <div className="line number1 index0 alt2"><code className="comments">/* C program for Merge Sort */</code></div>
