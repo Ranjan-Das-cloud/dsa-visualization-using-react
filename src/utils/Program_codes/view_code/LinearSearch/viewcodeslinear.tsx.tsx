0db4c91ffcc1b525b90af4b724be1bda9e6f91c8
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-02-03 14:16:18 +0530

    Modified CSS styles of title of code explorer

--- src/utils/Program_codes/view_code/LinearSearch/viewcodeslinear.tsx.tsx
+++ src/utils/Program_codes/view_code/LinearSearch/viewcodeslinear.tsx.tsx
@@ -5,16 +5,18 @@ import { Text } from "@react-md/typography";
 import CrossFadeExamplesAsync from "./CrossFadeElementAsyncLinear";
 //import CrossFadeExamplesStatic from "./CrossFadeExamplesStatic";
 
+import '../../../../sortingAlgorithms/__colorHub/codestyle.css';
+
 const Viewcodes: FC = () => (
   <>
     <Text type="headline-6" margin="bottom">
       <div className="mt-1"></div>
-      Code Explorer
+      <h3 className="text-white bg-primary badge font-bold mt-2">Code Explorer</h3>
     </Text>
     
-    <Divider />
+    <Divider style={{background: ''}} />
     <Text type="headline-6">
-      <span className="text-info">Source/Program codes in multiple </span><code>Programming Languages</code>
+    <span className="text-info badge bg-white font-sub-heading">Source/Program codes in multiple&nbsp;<span className="text-danger">Programming Languages</span></span>
     </Text>
     <CrossFadeExamplesAsync />
     <Divider />
