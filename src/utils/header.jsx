9da6ba96f5a1a9c096376eb4e72fc624f31b724b
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2021-02-03 13:52:04 +0530

    Modified color of Algorithm Visualizer title

--- src/utils/header.jsx
+++ src/utils/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = ({ title }) => (
     <div>
-        <h2 className="mt-3 text-center" >{title}</h2>
+        <h2 className="mt-3 text-white mb-3 text-center" ><strong>{title}</strong></h2>
     </div>
 );
 
